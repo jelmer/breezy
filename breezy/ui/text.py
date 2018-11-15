b249cdf561aaa6847af35f6ce934f14632cb8e2d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-11-15 22:30:32 +0000

    Use _unwrap_stream.

--- breezy/ui/text.py
+++ breezy/ui/text.py
@@ -67,7 +67,7 @@ class _ChooseUI(object):
         """
         is_tty = self.ui.raw_stdin.isatty()
         if (os.environ.get('BRZ_TEXTUI_INPUT') != 'line-based' and
-                self.ui.raw_stdin == getattr(sys.stdin, 'buffer', sys.stdin) and is_tty):
+                self.ui.raw_stdin == _unwrap_stream(sys.stdin) and is_tty):
             self.line_based = False
             self.echo_back = True
         else:
